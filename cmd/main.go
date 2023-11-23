3996e674654d7b2fe550be810346f67c4ec60c9f
acentior <risingdev000@gmail.com>
2023-11-23 04:53:16 +0200

    camera connect with pion

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"log"
+
+	"github.com/acentior/camera-pipeline-sender/pkg/sender"
 )
 
 func main() {
 	log.Printf("camera-pipeline-sender")
+
+	// s := sender.CameraVideoStreamSender{}
+	// s := sender.CameraVideoStreamSender{}
+	s := sender.CameraVideoStreamSender{}
+	s.ConnectCamera()
 }
